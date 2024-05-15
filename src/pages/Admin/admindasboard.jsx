fc76b0a7780a2b1c1828b5b7826a7daa078f3c7d
Hpatwareids <154405565+Hpatwareids@users.noreply.github.com>
2024-05-15 19:26:39 +0530

    search

--- src/pages/Admin/admindasboard.jsx
+++ src/pages/Admin/admindasboard.jsx
@@ -1,21 +1,19 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import axios from "axios";
 
-// import Rides from "./rides";
-// import Users from "./users";
- const AdminDashboard = () => {
+const AdminDashboard = () => {
   const [ride, setRides] = useState([]);
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery2, setSearchQuery2] = useState("");
 
   const userValid = () => {
     let token = localStorage.getItem("userdbtoken");
-    if (token) {
-      console.log("user valid");
-    } else {
+    if (!token) {
       navigate("*");
     }
   };
@@ -33,15 +31,17 @@ import axios from "axios";
     } catch (error) {
       console.error("Error fetching rides:", error);
     }
-  }
+  };
+
   const fetchUsers = async () => {
     try {
       const response = await axios.get("http://localhost:4002/user/register");
       setUsers(response.data);
     } catch (error) {
-      console.error("Error fetching rides:", error);
+      console.error("Error fetching users:", error);
     }
-  }
+  };
+
   const deleteRide = (id) => {
     const confirmed = window.confirm("Do you permanently want to delete the ride?");
     if (confirmed) {
@@ -52,17 +52,41 @@ import axios from "axios";
       });
     }
   };
+
   const deleteUser = (id) => {
-    const confirmed = window.confirm("Do you permanently want to delete the ride?");
+    const confirmed = window.confirm("Do you permanently want to delete the user?");
     if (confirmed) {
       axios.delete(`http://localhost:4002/user/register/${id}`).then(() => {
         setUsers(users.filter((user) => user._id !== id));
       }).catch((error) => {
-        console.error("Error deleting ride:", error);
+        console.error("Error deleting user:", error);
       });
     }
-  };  
+  };
 
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleInputChange2 = (e) => {
+    setSearchQuery2(e.target.value);
+  };
+
+  const filteredRides = ride.filter(
+    (ride) =>
+      ride.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.start.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.destination.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.route.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.startTime.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const filteredUsers = users.filter(
+    (user) =>
+      user.fname.toLowerCase().includes(searchQuery2.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchQuery2.toLowerCase()) ||
+      user.role.toLowerCase().includes(searchQuery2.toLowerCase())
+  );
 
   return (
     <>
@@ -76,26 +100,31 @@ import axios from "axios";
               style={{ maxHeight: "10rem", maxWidth: "10rem" }}
             />
           </div>
-          <div>
-            {/* <Link to={Users} >USERS</Link>
-            <Link to={Rides} >RIDES</Link> */}
-          </div>
-
           <div ml-auto flex>
             <h5 className="text-white">ADMIN DASHBOARD</h5>
           </div>
         </div>
       </nav>
 
-   <div className="m-4" style={{justifyContent:"center",display:"flex"}}>
-    <div>
-  <h3 style={{justifyContent:"center",display:"flex"}}> Welcome To Admin Dashboard </h3>
-  <p> This is the dashboard to maintain and manage the users and rides this can also used for managing roles. </p>
-  </div>
-</div>
+      <div className="m-4" style={{ justifyContent: "center", display: "flex" }}>
+        <div>
+          <h3 style={{ justifyContent: "center", display: "flex" }}> Welcome To Admin Dashboard </h3>
+          <p> This is the dashboard to maintain and manage the users and rides. It can also be used for managing roles. </p>
+        </div>
+      </div>
+
       {/* Ride List */}
-        <div className=' rounded shadow p-4 m-5 '>
-      <h2>Ride List</h2>
+      <div className='rounded shadow p-4 m-5'>
+        <div className="d-flex">
+          <h2>ALL RIDES LISTS</h2>{" "}
+          <input
+            className="mx-4 border px-2 mb-2 rounded shadow"
+            type="text"
+            placeholder="Search rides..."
+            value={searchQuery}
+            onChange={handleInputChange}
+          />
+        </div>
         <table className="table">
           <thead>
             <tr className='table-dark'>
@@ -105,11 +134,11 @@ import axios from "axios";
               <th>Destination</th>
               <th>Route</th>
               <th>Start Time</th>
-              <th>Request Ride</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {ride.map((ride, i) => (
+            {filteredRides.map((ride, i) => (
               <tr key={ride._id}>
                 <td scope="row">{i + 1}</td>
                 <td>{ride.name}</td>
@@ -118,8 +147,9 @@ import axios from "axios";
                 <td>{ride.route}</td>
                 <td>{ride.startTime}</td>
                 <td>
-                <button className="btn btn-dark btn-sm mx-2" >
-                <Link to={`/updateRide/${ride._id}`} style={{color:'white', textDecoration:"none"}}>Edit</Link></button>
+                  <button className="btn btn-dark btn-sm mx-2">
+                    <Link to={`/updateRide/${ride._id}`} style={{ color: 'white', textDecoration: "none" }}>Edit</Link>
+                  </button>
                   <button className="btn btn-success btn-sm" onClick={() => deleteRide(ride._id)}>Delete</button>
                 </td>
               </tr>
@@ -127,10 +157,19 @@ import axios from "axios";
           </tbody>
         </table>
       </div>
-       {/* User List */}
-      
+
+      {/* User List */}
       <div className='rounded shadow p-4 m-5'>
-      <h2>User List</h2>
+        <div className="d-flex">
+          <h2>ALL USERS </h2>{" "}
+          <input
+            className="mx-4 border px-2 mb-2 rounded shadow"
+            type="text"
+            placeholder="Search users..."
+            value={searchQuery2}
+            onChange={handleInputChange2}
+          />
+        </div>
         <table className="table">
           <thead>
             <tr className='table-dark'>
@@ -142,25 +181,27 @@ import axios from "axios";
             </tr>
           </thead>
           <tbody>
-            {users.map((user, i) => (
+            {filteredUsers.map((user, i) => (
               <tr key={user._id}>
                 <td scope="row">{i + 1}</td>
                 <td>{user.fname}</td>
                 <td>{user.email}</td>
                 <td>{user.role}</td>
                 <td>
-                  <button className="btn btn-dark btn-sm" >
-                    <Link to={`/update/${user._id}`} style={{color:'white',textDecoration:"none"}}>Edit</Link></button>
-                  <button className="btn btn-success btn-sm mx-2" onClick={() => deleteUser(user._id)}>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+                  <button className="btn btn-dark btn-sm">
+                    <Link to={`/update/${
 
-    </>
-  );
+user._id}`} style={{ color: 'white', textDecoration: "none" }}>Edit</Link>
+</button>
+<button className="btn btn-success btn-sm mx-2" onClick={() => deleteUser(user._id)}>Delete</button>
+</td>
+</tr>
+))}
+</tbody>
+</table>
+</div>
+</>
+);
 };
 
 export default AdminDashboard;
--- src/pages/User/dashboard.jsx
+++ src/pages/User/dashboard.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate , useParams} from 'react-router-dom'
-import axios from "axios";
-import { sendAuthorDetails } from "../../services/Apis";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
 import styles from '../../styles/styles.module.css';
 import logo from '../../assets/images/logo.png';
-import profileIcon from "../../assets/images/profileIcon.svg"
-import { Container,Row,Col, Button, } from "reactstrap";
-import Heroimg from "../../assets/images/herosection.png" 
- import { Img } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import profileIcon from '../../assets/images/profileIcon.svg';
+import { Container, Row, Col, Button } from 'reactstrap';
+import Heroimg from '../../assets/images/herosection.png';
+import { Img } from '@chakra-ui/react';
 import CreateRideModal from './CreateRideModal';
 
-const Dashboard = ({onRandomTokenChange, loginUser }) => {
-    const navigate = useNavigate();
+const Dashboard = ({ onRandomTokenChange, loginUser }) => {
+  const navigate = useNavigate();
   const [isPopoverOpen, setPopoverOpen] = useState(false);
   const [ride, setRide] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery2, setSearchQuery2] = useState('');
   const [personalRides, setPersonalRides] = useState([]);
-  const [randomToken, setRandomToken] = useState('');  
+  const [randomToken, setRandomToken] = useState('');
   const { token } = useParams();
-  const [loggedInUser, setLoggedInUser] =useState({});
+  const [loggedInUser, setLoggedInUser] = useState({});
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    setLoggedInUser(loginUser);
+  }, [loginUser]);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -28,106 +33,75 @@ const Dashboard = ({onRandomTokenChange, loginUser }) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  useEffect(()=>{
-    setLoggedInUser(loginUser);
-  })
 
-  console.log("login user in dashboard:", loggedInUser?.fname);
-
-  const [newRide, setNewRide] = useState({ 
-    name: loggedInUser?.fname || '',
-    start: "",
-    destination: "",
-    route: "",
-    startTime: "",
-  });
- 
-  useEffect(() => {
-    setNewRide(prevRide => ({
-      ...prevRide,
-      name: loggedInUser?.fname || "",
-    }));
-  }, [loggedInUser]);
-// userValidation and store JWT in local storage
   const userValid = () => {
-    let token = localStorage.getItem("userdbtoken");
-    if (token) {
-      console.log("user valid")
-    } else {
-      navigate("*")
+    let token = localStorage.getItem('userdbtoken');
+    if (!token) {
+      navigate('*');
     }
-  }
+  };
+
   useEffect(() => {
     userValid();
-  }, [])
-
-  // handle create ride popover
-  const handleButtonClick = (e) => {
-    e.preventDefault();
-    setPopoverOpen(!isPopoverOpen);
-  };
-
-// Save new ride
-  useEffect(() => {
-    axios
-      .get("http://localhost:4002/user/ride")
-      .then((response) => {
-        const allRides = response.data;
-        console.log(response.data)
-        const personalRides = allRides.filter((ride) => ride.name === 'User Kompal');
-        setRide(allRides);
-        setPersonalRides(personalRides);
-        console.log("Personal Rides:", personalRides);
-      });
-    
   }, []);
 
+  useEffect(() => {
+    axios.get('http://localhost:4002/user/ride').then((response) => {
+      const allRides = response.data;
+      const personalRides = allRides.filter((ride) => ride.name === 'User Kompal');
+      setRide(allRides);
+      setPersonalRides(personalRides);
+    });
+  }, []);
+
+  const filteredRides = ride.filter(
+    (ride) =>
+      ride.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.start.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.destination.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.route.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      ride.startTime.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const filteredPersonalRides = personalRides.filter(
+    (ride) =>
+      ride.name.toLowerCase().includes(searchQuery2.toLowerCase()) ||
+      ride.start.toLowerCase().includes(searchQuery2.toLowerCase()) ||
+      ride.destination.toLowerCase().includes(searchQuery2.toLowerCase()) ||
+      ride.route.toLowerCase().includes(searchQuery2.toLowerCase()) ||
+      ride.startTime.toLowerCase().includes(searchQuery2.toLowerCase())
+  );
+
+  const handleInputChange2 = (e) => {
+    setSearchQuery2(e.target.value);
+  };
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
-   
-    setNewRide((prevRide) => ({ ...prevRide, [name]: value }));
+    setSearchQuery(e.target.value);
   };
 
-  const handleAddEmployee = (event) => {
-    axios
-      .post("http://localhost:4002/user/ride", newRide)
-      .then((response) => {
-        setRide([...ride, response.data]);
-        setNewRide({
-          name: "",
-          start: "",
-          destination: "",
-          route: "",
-          startTime: "",
-        });
-      });
-      setPopoverOpen(!isPopoverOpen);
-      console.log("ride created successfully")
-    event.preventDefault();
-    
-  };
-
-
-  const requestRide = async (author) =>{
-    console.log("Requesting ride from:", author);
-    const requestRideDetails ={
-      fname : author
-    }
-    console.log(requestRideDetails)
+  const requestRide = async (author) => {
+    toast.success('Ride request sent!');
+    console.log('Requesting ride from:', author);
+    const requestRideDetails = {
+      fname: author,
+    };
     const response = await sendAuthorDetails(requestRideDetails);
-    // const confirmLinkToken = response.data.randomToken;
-    // console.log("The random token is:",confirmLinkToken);
-    
+
     const newRandomToken = response.data.randomToken;
-        console.log("Random token:", newRandomToken); // Log the token here
-        setRandomToken(newRandomToken);
-        onRandomTokenChange(newRandomToken);
+    console.log('Random token:', newRandomToken);
+    setRandomToken(newRandomToken);
+    onRandomTokenChange(newRandomToken);
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem('userdbtoken');
+    localStorage.removeItem('loggedUser');
+    navigate('/');
   };
 
   return (
-    <div >
-      {/* navbar */}
+    <div>
       <nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
         <div className="container-fluid d-flex bd-highlight">
           <div className="w-100 bd-highlight">
@@ -136,30 +110,35 @@ const Dashboard = ({onRandomTokenChange, loginUser }) => {
               <span className="navbar-toggler-icon"></span>
             </button>
           </div>
-          {/* user profile details modal */}
-          <h5 className='mx-2' style={{color:'white'}}>Hi! {loggedInUser?.fname}</h5>
-
+          <h5 className="mx-2" style={{ color: 'white' }}>
+            Hi! {loggedInUser?.fname}
+          </h5>
           <div className="collapse navbar-collapse flex-shrink-2 bd-highlight" id="navbarSupportedContent">
-
             <button type="button" className="btn btn-secondary profileBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">
               <img src={profileIcon} alt="" />
             </button>
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Hello { loggedInUser?.fname}</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">
+                      Hello {loggedInUser?.fname}
+                    </h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
-                  <div class="modal-body">
-                   <b> Personal Details </b><br />
-                    Name:{loggedInUser?.fname}<br />
+                  <div className="modal-body">
+                    <b> Personal Details </b><br />
+                    Name: {loggedInUser?.fname}<br />
                     email: {loggedInUser?.email} <br />
                     role: {loggedInUser?.role}
                   </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-success">Logout</button>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-outline-secondary" data-bs-dismiss="modal">
+                      Close
+                    </button>
+                    <button type="button" className="btn btn-success" onClick={logoutUser}>
+                      Logout
+                    </button>
                   </div>
                 </div>
               </div>
@@ -167,110 +146,108 @@ const Dashboard = ({onRandomTokenChange, loginUser }) => {
           </div>
         </div>
       </nav>
-
-
-      <Container className="mt-5 bg-white" >
-    <Row >
-        <Col className="col-5 mt-5 ">
-        <h2> RIDE. SHARE.</h2>
-        <h1> CONNECT </h1>    
-        <p>With RideShare, ditch cumbersome traffic jams and build networks as you go. Pool vehicles and serve the environment.</p>
-        <div >
-                    <button
-                      className="btn btn-outline-success w-full py-2 mt-2"
-                      onClick={openModal}
-                    >
-                      Create Ride
-                    </button>
-                    <CreateRideModal
-                      isOpen={isModalOpen}
-                      closeModal={closeModal}
-                      setisOpen={setIsModalOpen}
-                    />
-                  </div>
-        
-       
-        
-        </Col> 
-        <Col className="col-7">
-        <Img className="img-fluid" src={Heroimg} alt="" />
-        </Col>
-    </Row>
-</Container>
-
-      {/* create ride form */}
-   
-     {/* show all rides */}
-<div className=' rounded shadow p-4 m-5 '>
-<h2>ALL RIDES</h2>
-
-            <table class="table">
-                <thead>
-                    <tr className='table-dark'>
-                        <th>ID</th>
-                        <th>Author Name</th>
-                        <th>Start</th>
-                        <th>Destination</th>
-                        <th>Route</th>
-                        <th>Start Time</th>
-                        <th>Request Ride</th>
-                    </tr>
-                </thead>
-                <tbody>
-                
-                    {ride.map((ride,i=1) => (
-              <tr key={ride._id}>
-                <td scope="row">{i+1}</td>
-                <td>{ride.name}</td>
-                <td>{ride.start}</td>
-                <td>{ride.destination}</td>
-                <td>{ride.route}</td>
-                <td>{ride.startTime}</td>
-                <td>
-                  <button className="btn btn-outline-dark btn-sm " onClick={() => requestRide(ride.name)}>Send Request</button>
-                </td>
-             </tr>
-             ))}
-                </tbody>
-            </table>
+      <Container className="mt-5 bg-white">
+        <Row>
+          <Col className="col-5 mt-5 ">
+            <h2> RIDE. SHARE.</h2>
+            <h1> CONNECT </h1>
+            <p>With RideShare, ditch cumbersome traffic jams and build networks as you go. Pool vehicles and serve the environment.</p>
+            <div>
+              <button className="btn btn-outline-success w-full py-2 mt-2" onClick={openModal}>
+                Create Ride
+              </button>
+              <CreateRideModal isOpen={isModalOpen} closeModal={closeModal} setisOpen={setIsModalOpen} />
+            </div>
+          </Col>
+          <Col className="col-7">
+            <Img className="img-fluid" src={Heroimg} alt="" />
+          </Col>
+        </Row>
+      </Container>
+      <div className="rounded shadow p-4 m-5">
+        <div className="d-flex">
+          <h2>ALL RIDES</h2>{" "}
+          <input className="mx-4 border px-2 mb-2 rounded shadow" type="text" placeholder="Search rides..." value={searchQuery} onChange={handleInputChange} />
         </div>
-{/* Personal rides */}
-        <div className=' rounded shadow p-4 m-5 '>
-        <h2>MY RIDES</h2>
-
-            <table class="table">
-                <thead>
-                    <tr className='table-dark'>
-                        <th>ID</th>
-                        <th>Author Name</th>
-                        <th>Start</th>
-                        <th>Destination</th>
-                        <th>Route</th>
-                        <th>Start Time</th>
-                        <th>Request Ride</th>
-                    </tr>
-                </thead>
-                <tbody>
-                
-                    {personalRides.map((ride,i=1) => (
+        <table className="table">
+          <thead>
+            <tr className="table-dark">
+              <th>ID</
+              th>
+              <th>Author Name</th>
+              <th>Start</th>
+              <th>Destination</th>
+              <th>Route</th>
+              <th>Start Time</th>
+              <th>Request Ride</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredRides.map((ride, i) => (
               <tr key={ride._id}>
-                <td scope="row">{i+1}</td>
+                <td scope="row">{i + 1}</td>
                 <td>{ride.name}</td>
                 <td>{ride.start}</td>
                 <td>{ride.destination}</td>
                 <td>{ride.route}</td>
                 <td>{ride.startTime}</td>
                 <td>
-                  <button className="btn btn-outline-dark btn-sm ">Edit</button>
+                  <button
+                    className="btn btn-outline-dark btn-sm"
+                    onClick={() => requestRide(ride.name)}
+                  >
+                    Send Request
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="rounded shadow p-4 m-5">
+        <div className="d-flex">
+          <h2>MY RIDES</h2>{" "}
+          <input
+            className="mx-4 border px-2 mb-2 rounded shadow"
+            type="text"
+            placeholder="Search rides..."
+            value={searchQuery2}
+            onChange={handleInputChange2}
+          />
+        </div>{" "}
+        <table className="table">
+          <thead>
+            <tr className="table-dark">
+              <th>ID</th>
+              <th>Author Name</th>
+              <th>Start</th>
+              <th>Destination</th>
+              <th>Route</th>
+              <th>Start Time</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredPersonalRides.map((ride, i) => (
+              <tr key={ride._id}>
+                <td scope="row">{i + 1}</td>
+                <td>{ride.name}</td>
+                <td>{ride.start}</td>
+                <td>{ride.destination}</td>
+                <td>{ride.route}</td>
+                <td>{ride.startTime}</td>
+                <td>
+                  <button className="btn btn-outline-dark btn-sm">Edit</button>
                   <button className="btn btn-success btn-sm mx-2">Delete</button>
                 </td>
-             </tr>
-             ))}
-                </tbody>
-            </table>
-        </div>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
